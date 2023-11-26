1cfd2d167ecefac1fdd293e1741a88bfa45e21d7
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2023-11-26 12:31:15 -0500

    primera version filtrar

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -23,12 +23,21 @@ package object ReconstCadenas {
 
   }
 
-  def reconstruirCadenaTurboMejorado(n: Int, o: Oraculo): Seq[Char] = {
+  def reconstruirCadenaTurboMejorado(n: Int, o: Oraculo): Seq[Seq[Char]] =
+  {
     // Recibe la longitud de la secuencia que hay que reconstruir (n, potencia de 2),
     // y un oráculo para esa secuencia, y devuelve la secuencia reconstruida.
     // Usa la propiedad de que si s = s1 ++ s2 entonces s1 y s2 también son subsecuencias de s.
     // Usa el filtro para ir más rápido.
     // ...
+    def Filtrar(SC: Seq[Seq[Char]], k: Int): Seq[Seq[Char]] = {
+      for {
+        cadena1 <- SC
+        cadena2 <- SC
+        comprobante <- SC
+        if ((cadena1 ++ cadena2).slice(k / 2, k + (k / 2)) == comprobante)
+      } yield cadena1 ++ cadena2
+    }
   }
 
   def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
